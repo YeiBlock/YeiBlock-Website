9f9160b68ac3afd647861ba682748f87092c4f0a
YeiBlock <115025076+YeiBlock@users.noreply.github.com>
2023-07-02 17:55:41 -0400

    Update Contact.jsx
    Email services update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,13 +33,13 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_bt8f34r',
+        'template_xpkqe3o',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "YeiBlock",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "management@yeiblock.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
